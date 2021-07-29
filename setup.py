1d85252a14e7c56a371b3bbc0aee9111603a6ac3
Egil <egil.moller@freecode.no>
2021-07-29 10:49:43 +0200

    Rename

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import setuptools
 import os
 
 setuptools.setup(
-    name='libxyz',
+    name='libaarhusxyz',
     version='0.0.7',
     description='Parser for the Aarhus Workbench XYZ format for geophysical data',
     long_description="""Parser for the Aarhus Workbench XYZ format for geophysical data""",
     long_description_content_type="text/markdown",
     author='Egil Moeller, Craig William Christensen and others ',
     author_email='em@emeraldgeo.no, cch@emeraldgeo.no',
-    url='https://github.com/emerald-geomodelling/libxyz',
+    url='https://github.com/emerald-geomodelling/libaarhusxyz',
     packages=setuptools.find_packages(),
     install_requires=[
         "numpy",
