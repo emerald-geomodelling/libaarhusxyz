36db0f85d57d7bf0cb31bc8f235c305a2833445f
Martin Panzner <mp@emrld.no>
2022-04-28 16:08:00 +0200

    extended supported fields

--- libaarhusxyz/alc.py
+++ libaarhusxyz/alc.py
@@ -15,7 +15,15 @@ def parse(nameorfile, xyz_columns=None):
     return {"meta": meta,
             "mapping": mapping}
 
-supported_fields = ["Date","Line","Magnetic","Misc1 ","Misc2","Misc3","Misc4","PowerLineMonitor","RxPitch","RxRoll","Time","Topography","TxAltitude","TxOffTime","TxOnTime","TxPeakTime","TxPitch","TxRoll","TxRxHoriSep","TxRxVertSep","UTMX","UTMY","Current_Ch01","Current_Ch02","Gate_Ch01.*","Gate_Ch02.*","STD_Ch01.*","STD_Ch02.*"]
+supported_fields = ["DateTime","Date","Time","Flight","Line","GdSpeed","Alt","DEM",
+                    "Magnetic","PowerLineMonitor",
+                    "RxPitch","RxRoll","Topography","TxAltitude",
+                    "TxOffTime","TxOnTime","TxPeakTime",
+                    "TxPitch","TxRoll","TxRxHoriSep","TxRxVertSep","UTMX","UTMY",
+                    "Current_Ch01","Current_Ch02",
+                    "Gate_Ch01.*","Gate_Ch02.*",
+                    "STD_Ch01.*","STD_Ch02.*",
+                    "InUse_Ch01.*","InUse_Ch02.*"]
 
 
 def is_supported_field(fieldname):
