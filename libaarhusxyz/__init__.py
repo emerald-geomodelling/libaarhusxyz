cb665e6ea68f64239df5bc780426fd31ca064710
Martin Panzner <mp@emrld.no>
2022-10-25 10:43:19 +0200

    added gex file writer

--- libaarhusxyz/__init__.py
+++ libaarhusxyz/__init__.py
@@ -4,3 +4,4 @@ from .xyz import XYZ
 
 from .sr2 import parse as parse_sr2
 from .gex import parse as parse_gex
+from .gex import dump as dump_gex
--- libaarhusxyz/gex.py
+++ libaarhusxyz/gex.py
@@ -6,7 +6,7 @@ Created on Fri Oct 29 11:36:28 2021
 
 
 import numpy as np
-
+from datetime import datetime
 
 
 
@@ -32,7 +32,7 @@ def split_sections(text):
     return sections, sectionheaders
 
 def parse_parameters(textlines):
-    general={}
+    section={}
     keys=[]
     for line in textlines:
         strings=line.split("=")
@@ -45,19 +45,19 @@ def parse_parameters(textlines):
                 numbers=numberstrings
             
             if not( key in " ".join(keys) ) :
-                general[key]=[]
+                section[key]=[]
                 
-            general[key].append(numbers)
+            section[key].append(numbers)
             keys.append(key)
     
-    for key in general.keys():
-        if len(general[key])==1:
-            general[key]=general[key][0]
-        if len(general[key])==1:
-            general[key]=general[key][0]
+    for key in section.keys():
+        if len(section[key])==1:
+            section[key]=section[key][0]
+        if len(section[key])==1:
+            section[key]=section[key][0]
         else:
-            general[key]=np.array(general[key])
-    return general
+            section[key]=np.array(section[key])
+    return section
 
 def _parse(inputfile):
     sections, sectionheaders = split_sections(inputfile.readlines())
@@ -74,4 +74,58 @@ def parse(nameorfile, **kw):
     else:
         return _parse(nameorfile, **kw)
 
+def _dump(gex, f, columns=None):
+    lines=[]
+    
+    now = datetime.now() 
+    gex['header']='/{0} gex file modified by the EmeraldProcessing toolbox '.format(now.strftime('%Y-%m-%d'))
+    lines.append(gex['header'])
+    
+    gex_keys=(list(gex.keys()))
+    gex_keys.remove('header')
+    
+    for key1 in gex_keys:
+        #for key1 in ['General']:
+        lines.append('\n[{}]'.format(key1))
+        for key2 in gex[key1].keys():
+            if 'General' in key1:
+                lines.append('\n')
+            a=gex[key1][key2]
+            if type(a) is np.ndarray:
+                if type(a[0]) is np.str_:
+                    lines.append('{0}={1}'.format(key2, ' '.join(['{}'.format(item) for item in a[:] ]))) 
+                elif a.ndim==1:
+                    digits=1
+                    if 'TiBLowPassFilter' in key2:
+                        lines.append('{0}=\t {1}'.format(key2, '\t'.join(['{:.2f}'.format(item) for item in a[:] ])) )
+                    elif 'Position' in key2:
+                        lines.append('{0}{1}=\t {2}'.format(key2, 1, '\t'.join(['{:.2f}'.format(item) for item in a[:] ])) )
+                    else:
+                        lines.append('{0}{1}=\t {2}'.format(key2, 1,'\t'.join(['{:f}'.format(item) for item in a[:] ])) )
+                elif a.ndim==2:
+                    digits=int(np.ceil(np.log10(a.shape[0])))
+                    for n,row in enumerate(a):
+                        if ('Waveform' in key2)  or ('GateTime' in key2):
+                            lines.append(('{0}{1:0'+str(digits)+'d}=\t{2}').format(key2, n+1, '\t'.join(['{:e}'.format(item) for item in row ])) )
+                        elif 'Position' in key2:
+                            lines.append(('{0}{1:0'+str(digits)+'d}=\t{2}').format(key2, n+1, '\t'.join(['{:.2f}'.format(item) for item in row ])) )
+                        else:
+                            lines.append(('{0}{1:0'+str(digits)+'d}=\t{2}').format(key2, n+1, '\t'.join(['{}'.format(item) for item in row ])) )
+            elif type(a) is float:
+                if key2 in ['GateNoForPowerLineMonitor', 'RemoveInitialGates', 'NumberOfTurnsLM', 'NumberOfTurnsHM','LoopType' , 'RxCoilNumber', 'NoGates' ]:
+                    lines.append('{0}={1}'.format(key2, int(a)))
+                else:
+                    lines.append('{0}={1}'.format(key2, a))
+            elif type(a) is str:
+                lines.append('{0}={1}'.format(key2, a))
+    
+    for line in lines:
+        f.write('{0}\n'.format(line))
+
+def dump(gex, nameorfile, **kw):
+    if isinstance(nameorfile, str):
+        with open(nameorfile, 'w') as f:
+            return _dump(gex, f, **kw)
+    else:
+        return _dump(gex, nameorfile, **kw)
 
