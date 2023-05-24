a6f2e16696c03c5b4fd7a4a6cdfadfb3dbb28bb4
Egil <egil.moller@freecode.no>
2023-05-24 14:22:29 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name='libaarhusxyz',
-    version='0.0.6',
+    version='0.0.7',
     description='Parser for the Aarhus Workbench XYZ format for geophysical data',
     long_description="""Parser for the Aarhus Workbench XYZ format for geophysical data""",
     long_description_content_type="text/markdown",
