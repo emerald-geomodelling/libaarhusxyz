4ae556d344b026563b92d42660f61dff14489bb7
martineho <martineolsenn@hotmail.com>
2023-05-26 08:37:48 +0000

    Updated versions of dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name='libaarhusxyz',
-    version='0.0.7',
+    version="0.11.6",
     description='Parser for the Aarhus Workbench XYZ format for geophysical data',
     long_description="""Parser for the Aarhus Workbench XYZ format for geophysical data""",
     long_description_content_type="text/markdown",
