2a2bdbd44209413b45c7fbdd8282f93a39e56399
Egil <egil.moller@freecode.no>
2023-10-09 14:15:41 +0200

    Convert from msgpack

--- libaarhusxyz/export/msgpack.py
+++ libaarhusxyz/export/msgpack.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import msgpack
 import msgpack_numpy as m
 m.patch()
@@ -24,6 +25,15 @@ def dfs2dict(d):
     else:
         return d
 
+def dict2dfs(d):
+    if isinstance(d, dict):
+        if min([isinstance(col, np.ndarray) for col in d.values()]):
+            return pd.DataFrame(d)
+        else:
+            return {key: dict2dfs(value) for key, value in d.items()}
+    else:
+        return d
+    
 def _dump(model, f, gex=None):
     data = dict(model.model_dict)
     if gex is not None:
@@ -38,3 +48,25 @@ def dump(model, nameorfile, gex=None):
             _dump(model, f, gex=gex)
     else:
         _dump(model, nameorfile, gex=gex)
+
+def _load(f, return_gex=False):
+    from .. import xyz
+    from .. import gex
+    data = msgpack.load(f, strict_map_key=False)
+    data = dict2dfs(data)
+    system = data.pop("system", None)
+    data.pop("geojson", None)
+    model = xyz.XYZ(data)
+    if not return_gex:
+        return model
+    if system is not None:
+        system = gex.GEX(system)
+    return model, system
+
+def load(nameorfile, return_gex=False):
+    if isinstance(nameorfile, str):
+        with open(nameorfile, 'rb') as f:
+            return _load(f, return_gex)
+    else:
+        return _load(nameorfile, return_gex)
+
