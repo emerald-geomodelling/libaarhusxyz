8b52955987b4ec436bcf1f3b48016cef94154116
Benjamin R. Bloss <bloss.benjamin@gmail.com>
2024-09-13 11:47:59 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name='libaarhusxyz',
-    version='0.0.30',
+    version='0.0.31',
     description='Parser for the Aarhus Workbench XYZ format for geophysical data',
     long_description="""Parser for the Aarhus Workbench XYZ format for geophysical data""",
     long_description_content_type="text/markdown",
