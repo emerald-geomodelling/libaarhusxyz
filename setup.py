d5f124ebc516188b26f4d05424773f924a2195ba
Duke-of-Lizard <cch@emeraldgeo.no>
2026-01-29 19:49:33 +0100

    Updated versions of dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name='libaarhusxyz',
-    version='0.0.33',
+    version="0.17.9",
     description='Parser for the Aarhus Workbench XYZ format for geophysical data',
     long_description="""Parser for the Aarhus Workbench XYZ format for geophysical data""",
     long_description_content_type="text/markdown",
